a38b3b767b38c0dac5d2723939c094611127785e
Kaehaerae <kaharatopias@gmail.com>
2023-02-17 23:22:03 +0200

    Tunnit merkitty

--- Kanban/obj/Debug/net6.0-windows/Tehtavanluonti.g.i.cs
+++ Kanban/obj/Debug/net6.0-windows/Tehtavanluonti.g.i.cs
@@ -101,7 +101,7 @@ namespace Kanban {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Kanban;component/tehtavanluonti.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Kanban;V1.0.0.0;component/tehtavanluonti.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Tehtavanluonti.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
